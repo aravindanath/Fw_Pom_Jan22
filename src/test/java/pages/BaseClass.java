08eccb60fabb6376cf1621a33e640795c1e7116f
Raghava <raghavarohit0583@gmail.com>
2023-01-22 18:30:04 +0530

    ADDINGBASECLASS

--- src/test/java/pages/BaseClass.java
+++ src/test/java/pages/BaseClass.java
@@ -19,78 +19,80 @@ public class BaseClass {
 
     public BaseClass(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
 
-    public static void scroll(WebDriver driver, WebElement element){
+    public static void scroll(WebDriver driver, WebElement element) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView();",element);
+        js.executeScript("arguments[0].scrollIntoView();", element);
     }
 
-    public static void mouseHover(WebDriver driver, WebElement element){
+    public static void mouseHover(WebDriver driver, WebElement element) {
         Actions act = new Actions(driver);
         act.moveToElement(element).build().perform();
     }
-    public static void click(WebDriver driver, WebElement element){
+
+    public static void click(WebDriver driver, WebElement element) {
         Actions act = new Actions(driver);
         act.click(element).build().perform();
     }
 
 
-    public static void dragANDDrop(WebDriver driver, WebElement element1, WebElement element2){
+    public static void dragANDDrop(WebDriver driver, WebElement element1, WebElement element2) {
         Actions act = new Actions(driver);
-        act.dragAndDrop(element1,element2).build().perform();
+        act.dragAndDrop(element1, element2).build().perform();
     }
 
-    public static void takeScreenshot(WebDriver driver){
+    public static void takeScreenshot(WebDriver driver) {
         try {
             TakesScreenshot srcShot = (TakesScreenshot) driver;
             File output = srcShot.getScreenshotAs(OutputType.FILE);
             File path = new File("./screenshots.png");
             FileUtils.copyFile(output, path);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static String getValue(String key){
-        String path = System.getProperty("user.dir")+File.separator+"config.properties";
-        String val =null;
+
+    public static String getValue(String key) {
+        String path = System.getProperty("user.dir") + File.separator + "config.properties";
+        String val = null;
         try {
             FileInputStream fis = new FileInputStream(path);
             Properties prop = new Properties();
             prop.load(fis);
             val = prop.getProperty(key);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return val;
     }
 
 
-    public static String getValue(String title, String key){
-        String path = System.getProperty("user.dir")+ File.separator+"testdata"+File.separator+"data.ini";
-        String value =null;
+    public static String getValue(String title, String key) {
+        String path = System.getProperty("user.dir") + File.separator + "testdata" + File.separator + "data.ini";
+        String value = null;
         try {
             FileInputStream fis = new FileInputStream(path);
             Ini ini = new Ini();
             ini.load(fis);
             value = ini.get(title, key);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return value;
     }
 
-    public static String firstName(){
+    public static String firstName() {
         Faker faker = new Faker();
-       String fn =  faker.name().firstName();
-       return fn;
+        String fn = faker.name().firstName();
+        return fn;
     }
 
-    public static String lastName(){
+    public static String lastName() {
         Faker faker = new Faker();
-        String ln =  faker.name().lastName();
+        String ln = faker.name().lastName();
         return ln;
     }
 
